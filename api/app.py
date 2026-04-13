9ce40db3601374e953724c0742c611e54c40800f
Gabriel <saraiva.gabrielambrosio@gmail.com>
2026-04-13 20:32:15 -0300

    variaveis de ambiente

--- api/app.py
+++ api/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
+import os
+import oracledb
 
 app = Flask(__name__)
+
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_DSN = os.environ["DB_DSN"]
+
 
 @app.route('/')
 def home():
@@ -14,13 +21,28 @@ def inscrever():
     tipo = request.form.get('tipo')
     prioridade = request.form.get('prioridade')
 
-    # Aqui depois vamos conectar Oracle + PL/SQL
-    print(nome, email, tipo, prioridade)
+    try:
+        conn = oracledb.connect(
+            user=DB_USER,
+            password=DB_PASSWORD,
+            dsn=DB_DSN
+        )
 
-    return render_template(
-        'index.html',
-        mensagem=f'Inscrição de {nome} realizada com sucesso!'
-    )
+        cursor = conn.cursor()
+
+        # teste inicial
+        print("Conectado ao Oracle com sucesso")
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        mensagem = f'Inscrição de {nome} realizada com sucesso!'
+
+    except oracledb.DatabaseError as e:
+        mensagem = f'Erro Oracle: {str(e)}'
+
+    return render_template('index.html', mensagem=mensagem)
 
 
 if __name__ == '__main__':
